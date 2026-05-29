6ac80478593a1ef97701c9d2cc66d9b3be4f4169
kentranus <kentranus@gmail.com>
2026-05-29 12:49:56 -0400

    Delete vido-foody-flutter directory

--- vido-foody-flutter/test/widget_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:vido_foody_flutter/main.dart';
-
-void main() {
-  testWidgets('Vido Foody POS app loads', (tester) async {
-    await tester.pumpWidget(const VidoFoodyApp());
-    expect(find.text('Sell'), findsOneWidget);
-  });
-}
